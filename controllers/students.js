b9c13c018390cf7f330f8a01471dfe9b685e5196
MiAwan'sDev <103881058+MiAwan3332@users.noreply.github.com>
2026-09-23 21:08:58 +0500

    fixes into export student option

--- controllers/students.js
+++ controllers/students.js
@@ -1040,7 +1040,7 @@ export const getStudentsByBatch = async (req, res) => {
         page: parseInt(req.query.page),
         limit: parseInt(req.query.limit),
         populate: ["batch"],
-        sort: { roll_number: 1 },
+        sort: { roll_number: 1, name: 1, _id: 1 },
       });
 
     // Generate finance history if exporting (large limit)
